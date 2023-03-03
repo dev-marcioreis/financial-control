64d6d3bf56cf1b50fc6bdccd1ab7af1b40f28cdb
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-03 12:38:05 -0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import './header.css'
+import { TbPigMoney as Money } from 'react-icons/tb'
 
 const Header = () => {
   return (
     <>
-        <section className="header">
+        <section className="header flex">
             <div className="container">
-                <h1>Header</h1>
+              <h2><span><Money /></span> controle financeiro</h2>
             </div>
         </section>
     </>

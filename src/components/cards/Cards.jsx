f801a4cb0cbf65ecf7ea36455e85a873c3962da6
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-03 13:37:09 -0300

    cards

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,9 +1,18 @@
 import './cards.css'
 
-const Cards = () => {
+const Cards = ( { title, icon, total, amount } ) => {
   return (
     <>
-        <h1>Cards</h1>
+        <div className="cards flex">
+          <div className="card-header flex">
+            <h3>{title}</h3>
+            <small>{icon}</small>
+          </div>
+          <div className='total flex'>
+            <span>{total}</span>
+            <span>{amount}</span>
+          </div>
+        </div>
     </>
   )
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,16 @@
 import Cards from '../cards/Cards'
 import './main.css'
+import { AiOutlineArrowUp as Up, AiOutlineArrowDown as Down } from 'react-icons/ai'
+import { TbPigMoney as Money } from 'react-icons/tb'
 
 const Main = () => {
   return (
     <>
         <section className="section main">
             <div className="container flex">
-                <Cards />
-                <Cards />
-                <Cards />
+                <Cards title='crédito' icon={<Up />} total='total' amount='100,00' />
+                <Cards title='débito' icon={<Down />} total='total' amount='100,00' />
+                <Cards title='total' icon={<Money />} total='total' amount='100,00' />
             </div>
         </section>
     </>

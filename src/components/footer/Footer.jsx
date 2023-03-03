99fb6a9f9e6ac3785273f807fbabd18901d0161e
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-03 12:22:36 -0300

    create components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <>
+        <section className="section main">
+            <div className="container">
+                <h1>Footer</h1>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Footer

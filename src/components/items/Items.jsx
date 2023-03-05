ab4b2168bec620e19d7d01d53538032066706854
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-05 15:04:02 -0300

    media query

--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -14,19 +14,15 @@ const Items = ( { items, setItems } ) => {
   return (
     <>
         <section className='section items'>
-          <div className='container'>
               <div className='items__info'>
                 <span>descrição</span>
                 <span>valor</span>
                 <span>tipo</span>
                 <span>deletar</span>
               </div>
-              <div className='items-item'>
                 {
                   items?.map((item, index) => (<Item key={index} item={item} />))
                 }
-              </div>
-          </div>
         </section>
     </>
   )
--- src/components/items/item/Item.jsx
+++ src/components/items/item/Item.jsx
@@ -5,15 +5,13 @@ import { BsFillArrowUpCircleFill as Up, BsFillArrowDownCircleFill as Down } from
 const Item = ( { item, onDelete } ) => {
   return (
     <>
-        <section className='section item'>
-          <div className='container'>
+        <section className='item'>
               <div className='item__info'>
                 <span>{item.desc}</span>
                 <span>{`R$ ${item.amount}`}</span>
                 <span>{item.expense ? (<Down color="hsl(349, 98%, 58%)" />) : (<Up color="hsl(192, 98%, 47%)" />)}</span>
                 <span className='btn'>{<BsTrash color="hsl(349, 98%, 58%)" onClick={() => onDelete(item.id)}/>}</span>
               </div>
-          </div>
         </section>
     </>
   )
